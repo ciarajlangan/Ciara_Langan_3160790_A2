dac85ee93eeaf82b3cdf5f0c27c940103abb6e74
Ciara Langan <you@example.com>
2026-04-30 14:58:04 +0100

    Extended original homepage to present the options: Add, Search, Update and Delete. -> got rid of buttons to match proper Next.js app -> converted to ensure the use of only tailwind styling.

--- app/page.js
+++ app/page.js
@@ -4,21 +4,34 @@ import Link from "next/link";
 export default function Home() {
   return (
      <div className="text-gray-900 font-medium p-6 max-w-2xl mx-auto space-y-6">
-     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1 className="text-3xl font-bold mb-6">Welcome to My Assignment</h1>
-      <p>Select a section to continue:</p>
 
-      <div style={{ marginTop: "30px" }}>
-        <Link href="/part-a">
-          <button style={{ margin: "10px", padding: "10px 20px" }}>
-            Cinema Booking (Part A)
-          </button>
+     <div className= "text-centre, mt-12" >  
+      <h1 className="text-3xl font-bold mb-6">Welcome to My Assignment</h1>
+      <p className = "mb-6">Appliance Management System:</p>
+
+      <div className = "flex flex-col gap-4 items-center">
+        <Link 
+            href="/part-b-c/add"
+            className ="px-6 py-3 bg-blue-600 text-white rounded-xl hover: bg-blue-700">
+            Add Appliance
         </Link>
 
-        <Link href="/part-b-c">
-          <button style={{ margin: "10px", padding: "10px 20px" }}>
-            Appliance Inventory (Part B & C)
-          </button>
+        <Link 
+            href="/part-b-c/search"
+            className ="px-6 py-3 bg-green-600 text-white rounded-xl hover: bg-green-700">
+            Search Appliance
+        </Link>
+
+        <Link
+             href="/part-b-c/update"
+             className ="px-6 py-3 bg-yellow-600 text-white rounded-xl hover: bg-yellow-700">
+            Update Appliance
+        </Link>
+
+        <Link
+             href="/part-b-c/delete"
+             className ="px-6 py-3 bg-red-600 text-white rounded-xl hover: bg-red-700">
+            Delete Appliance
         </Link>
       </div>
     </div>
